527630025b0cd1150eed1c4eff8e44335bdf408e
karthikn08 <nandulakarthik01@gmail.com>
2023-12-22 11:01:09 +0530

    1

--- fullstack/data base/mangoooooooo/create.ts
+++ fullstack/data base/mangoooooooo/create.ts
@@ -4,15 +4,15 @@ var dev_url="mongodb://127.0.0.1:27017"
 var client=new MongoClient(dev_url)
 
 
-var dbName="practise_1stBatch"
+var dbName="2nd-data-base"
 
 var dbconnector=async()=>{
     await client.connect()
-    console.log("db connected"  )
+    console.log("db connected" )
    let db= client.db(dbName)
    let col=db.collection('student')
    let data={name:'abiya',choxalate:'dairymilk'}
    col.insertOne(data)
    console.log("val inseerted")
 }
-dbconnector()
+dbconnector() 
---
+++ fullstack/data base/mangoooooooo/delete.ts
@@ -0,0 +1,17 @@
+var {MongoClient}=require('mongodb')
+var dev_url="mongodb://127.0.0.1:27017"
+
+var client=new MongoClient(dev_url)
+
+var dbName="practise_1stBatch"
+
+var dbconnector=async()=>{
+    await client.connect()
+    console.log("db connected"  )
+   let db= client.db(dbName)
+   let col=db.collection('student')
+  
+   let data=col.deleteOne({name:'abiya'})
+   console.log(await data)
+}
+dbconnector() 
